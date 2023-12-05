a6f26b33baf70cfee6dfab16756fecdf1c9eb994
Andee <andeeolayinkascott@gmail.com>
2023-12-05 15:46:57 +0000

    Edit object key

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 export default function getListStudents() {
   const studentsList = [];
-  const names = ['Guilliame', 'James', 'Serena'];
+  const firstNames = ['Guilliame', 'James', 'Serena'];
   const id = [1, 2, 5];
   const locate = ['San Francisco', 'Columbia', 'San Francisco'];
 
-  for (let i = 0; i < names.length; i += 1) {
+  for (let i = 0; i < firstNames.length; i += 1) {
     const obj = {
       id: id[i],
-      name: names[i],
+      firstName: firstNames[i],
       location: locate[i],
     };
     studentsList.push(obj);
